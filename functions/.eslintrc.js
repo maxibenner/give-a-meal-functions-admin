bda4f4b62065f076c0ba41e1fa175649a5ce9f2e
maxibenner <maxibenner@gmail.com>
2023-06-19 12:32:38 -0400

    Fix: .eslint style to adhere to eslint rules

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -28,6 +28,6 @@ module.exports = {
   rules: {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
-    '@typescript-eslint/no-var-requires': 0
+    "@typescript-eslint/no-var-requires": 0
   }
 };
